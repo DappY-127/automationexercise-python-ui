3ef33c85a3fec49bd7bf0d85b5574f7ef432d1d4
Paul <paul.mshtk.707@gmail.com>
2024-01-30 13:23:59 +0200

    update is_opened, add screenshot making if faills

--- pages/base_page.py
+++ pages/base_page.py
@@ -26,11 +26,15 @@ class BasePage(HeaderFooterElements):
     def is_opened(self):
         # with allure.step(f"Page {self.PAGE_URL} is opened"):
         #     self.wait.until(EC.url_to_be(self.PAGE_URL))
+
         try:
             with allure.step(f"Page {self.PAGE_URL} is opened"):
+                self.check_and_close_ad_if_present()
+                # self.wait.until_not(EC.url_contains("#google_vignette"))
                 self.wait.until(EC.url_to_be(self.PAGE_URL))
         except Exception as e:
             current_url = self.browser.current_url
+            self.make_screenshot("Opened page")
             allure.attach(f"Test failed. Exception: {str(e)}. Current URL: {current_url}", name="Test Failure Details", attachment_type=allure.attachment_type.TEXT)
             raise
 
